911f4a34fd19edc3c59faa48d25eb481e985c9e5
Denis Moskovchenko <moskovchenkody@gmail.com>
2017-06-25 11:34:46 +0300

    рефакторинг стратегии

--- test_wood.py
+++ test_wood.py
@@ -5,40 +5,216 @@ import StringIO
 import itertools
 import random
 
+from woodapp import GameAction, GamePlayer, World, StrategyWood
 
 import woodapp as app
 
 class TestWorld(unittest.TestCase):
 
+    def test_init_player(self):
+        "Тестирование объекта Player"
+
+        player = GamePlayer(0, "3 3")
+        action = GameAction("MOVE&BUILD 0 E E")
+
+        self.assertEqual(player.future_move(action), (4, 3))
+        self.assertEqual(player.future_build(action), (5, 3))
+
+    def test_init_game_action(self):
+        "Тестирование преобразования в объект и обратно"
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 N E")), "MOVE&BUILD 0 N E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 N N")), "MOVE&BUILD 0 N N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 N NW")), "MOVE&BUILD 0 N NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 N S")), "MOVE&BUILD 0 N S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 N SW")), "MOVE&BUILD 0 N SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 N W")), "MOVE&BUILD 0 N W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NE NW")), "MOVE&BUILD 0 NE NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NE SE")), "MOVE&BUILD 0 NE SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NE SW")), "MOVE&BUILD 0 NE SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NE W")), "MOVE&BUILD 0 NE W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NW E")), "MOVE&BUILD 0 NW E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NW N")), "MOVE&BUILD 0 NW N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NW NE")), "MOVE&BUILD 0 NW NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NW NW")), "MOVE&BUILD 0 NW NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NW S")), "MOVE&BUILD 0 NW S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 NW SE")), "MOVE&BUILD 0 NW SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 S E")), "MOVE&BUILD 0 S E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 S N")), "MOVE&BUILD 0 S N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 S NW")), "MOVE&BUILD 0 S NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 S S")), "MOVE&BUILD 0 S S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 S SW")), "MOVE&BUILD 0 S SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 S W")), "MOVE&BUILD 0 S W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SE NE")), "MOVE&BUILD 0 SE NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SE NW")), "MOVE&BUILD 0 SE NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SE SW")), "MOVE&BUILD 0 SE SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SE W")), "MOVE&BUILD 0 SE W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SW E")), "MOVE&BUILD 0 SW E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SW N")), "MOVE&BUILD 0 SW N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SW NE")), "MOVE&BUILD 0 SW NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SW S")), "MOVE&BUILD 0 SW S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SW SE")), "MOVE&BUILD 0 SW SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SW SW")), "MOVE&BUILD 0 SW SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 SW W")), "MOVE&BUILD 0 SW W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 W E")), "MOVE&BUILD 0 W E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 W N")), "MOVE&BUILD 0 W N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 W NE")), "MOVE&BUILD 0 W NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 W S")), "MOVE&BUILD 0 W S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 W SE")), "MOVE&BUILD 0 W SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 0 W SW")), "MOVE&BUILD 0 W SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 E N")), "MOVE&BUILD 1 E N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 E NE")), "MOVE&BUILD 1 E NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 E S")), "MOVE&BUILD 1 E S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 E SE")), "MOVE&BUILD 1 E SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 E SW")), "MOVE&BUILD 1 E SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 E W")), "MOVE&BUILD 1 E W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NE E")), "MOVE&BUILD 1 NE E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NE N")), "MOVE&BUILD 1 NE N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NE NE")), "MOVE&BUILD 1 NE NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NE NW")), "MOVE&BUILD 1 NE NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NE S")), "MOVE&BUILD 1 NE S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NE SW")), "MOVE&BUILD 1 NE SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NW NE")), "MOVE&BUILD 1 NW NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NW S")), "MOVE&BUILD 1 NW S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NW SE")), "MOVE&BUILD 1 NW SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 NW SW")), "MOVE&BUILD 1 NW SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 S E")), "MOVE&BUILD 1 S E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 S N")), "MOVE&BUILD 1 S N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 S NE")), "MOVE&BUILD 1 S NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 S NW")), "MOVE&BUILD 1 S NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 S S")), "MOVE&BUILD 1 S S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 S SE")), "MOVE&BUILD 1 S SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 S W")), "MOVE&BUILD 1 S W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SE E")), "MOVE&BUILD 1 SE E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SE N")), "MOVE&BUILD 1 SE N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SE NW")), "MOVE&BUILD 1 SE NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SE S")), "MOVE&BUILD 1 SE S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SE SE")), "MOVE&BUILD 1 SE SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SE SW")), "MOVE&BUILD 1 SE SW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SE W")), "MOVE&BUILD 1 SE W")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SW E")), "MOVE&BUILD 1 SW E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SW N")), "MOVE&BUILD 1 SW N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SW NE")), "MOVE&BUILD 1 SW NE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SW NW")), "MOVE&BUILD 1 SW NW")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 SW SE")), "MOVE&BUILD 1 SW SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 W E")), "MOVE&BUILD 1 W E")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 W N")), "MOVE&BUILD 1 W N")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 W S")), "MOVE&BUILD 1 W S")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 W SE")), "MOVE&BUILD 1 W SE")
+        self.assertEqual(str(GameAction("MOVE&BUILD 1 W W")), "MOVE&BUILD 1 W W")
+        self.assertEqual(str(GameAction("PUSH&BUILD 0 E E")), "PUSH&BUILD 0 E E")
+        self.assertEqual(str(GameAction("PUSH&BUILD 1 N N")), "PUSH&BUILD 1 N N")
+        self.assertEqual(str(GameAction("PUSH&BUILD 1 N NE")), "PUSH&BUILD 1 N NE")
+
     def test_world_init(self):
         "Расчет направления"
 
-        sys.stdin = StringIO.StringIO("""5
-        2
-        MOLECULES 0 0 1 0 0 0 3 0 0 0 0 0
-        4 3 4 4 2
-        6
-        0 0 1 E 1 0 0 1 3 1
-        2 0 1 D 1 0 2 1 0 0
-        4 0 1 E 1 1 2 1 1 0
-        1 1 1 E 1 2 0 2 0 0
-        3 1 1 B 1 0 0 0 0 3
-        5 1 1 B 1 1 0 0 0 2""")   
+        sys.stdin = StringIO.StringIO("""7
+2
+...0...
+..000..
+.00000.
+0000000
+.00000.
+..000..
+...0...
+3 3
+1 3
+4 4
+2 1
+78
+MOVE&BUILD 0 E E
+MOVE&BUILD 0 E N
+MOVE&BUILD 0 E NE
+MOVE&BUILD 0 E NW
+MOVE&BUILD 0 E SE
+MOVE&BUILD 0 E SW
+MOVE&BUILD 0 E W
+MOVE&BUILD 0 N E
+MOVE&BUILD 0 N N
+MOVE&BUILD 0 N NE
+MOVE&BUILD 0 N S
+MOVE&BUILD 0 N SE
+MOVE&BUILD 0 N SW
+MOVE&BUILD 0 N W
+MOVE&BUILD 0 NE E
+MOVE&BUILD 0 NE N
+MOVE&BUILD 0 NE NW
+MOVE&BUILD 0 NE S
+MOVE&BUILD 0 NE SE
+MOVE&BUILD 0 NE SW
+MOVE&BUILD 0 NE W
+MOVE&BUILD 0 NW E
+MOVE&BUILD 0 NW NE
+MOVE&BUILD 0 NW S
+MOVE&BUILD 0 NW SE
+MOVE&BUILD 0 NW W
+MOVE&BUILD 0 S N
+MOVE&BUILD 0 S NE
+MOVE&BUILD 0 S NW
+MOVE&BUILD 0 S S
+MOVE&BUILD 0 S SE
+MOVE&BUILD 0 S SW
+MOVE&BUILD 0 S W
+MOVE&BUILD 0 SW E
+MOVE&BUILD 0 SW N
+MOVE&BUILD 0 SW NE
+MOVE&BUILD 0 SW S
+MOVE&BUILD 0 SW SE
+MOVE&BUILD 0 SW W
+MOVE&BUILD 0 W E
+MOVE&BUILD 0 W N
+MOVE&BUILD 0 W NE
+MOVE&BUILD 0 W NW
+MOVE&BUILD 0 W S
+MOVE&BUILD 0 W SE
+MOVE&BUILD 0 W SW
+MOVE&BUILD 1 E N
+MOVE&BUILD 1 E NE
+MOVE&BUILD 1 E NW
+MOVE&BUILD 1 E S
+MOVE&BUILD 1 E SE
+MOVE&BUILD 1 E SW
+MOVE&BUILD 1 E W
+MOVE&BUILD 1 N E
+MOVE&BUILD 1 N S
+MOVE&BUILD 1 N SE
+MOVE&BUILD 1 N SW
+MOVE&BUILD 1 NE E
+MOVE&BUILD 1 NE NE
+MOVE&BUILD 1 NE S
+MOVE&BUILD 1 NE SW
+MOVE&BUILD 1 NE W
+MOVE&BUILD 1 S E
+MOVE&BUILD 1 S N
+MOVE&BUILD 1 S NE
+MOVE&BUILD 1 S NW
+MOVE&BUILD 1 S SE
+MOVE&BUILD 1 SE E
+MOVE&BUILD 1 SE N
+MOVE&BUILD 1 SE NW
+MOVE&BUILD 1 SE S
+MOVE&BUILD 1 SE SE
+MOVE&BUILD 1 SE W
+MOVE&BUILD 1 W E
+MOVE&BUILD 1 W NE
+MOVE&BUILD 1 W SE
+PUSH&BUILD 0 SE E
+PUSH&BUILD 0 SE S""")
+        w = World()
 
+        w.update()
 
-        player = (5,3)
-        direction = (-1,-1)
-        pos = app.future_position(player, direction)
-        self.assertEqual(pos, (4,2))
+        print w.maps()
+
 
     def test_from_dir(self):
         "Расчет направления"
-        self.assertEqual(app.from_dir("NW"), (-1, -1))
-        self.assertEqual(app.from_dir("N"), (0, -1))
-        self.assertEqual(app.from_dir("SW"), (-1, 1))
-        self.assertEqual(app.from_dir("S"), (0, 1))
-        self.assertEqual(app.from_dir("E"), (1, 0))
-
+        # self.assertEqual(app.from_dir("NW"), (-1, -1))
+        # self.assertEqual(app.from_dir("N"), (0, -1))
+        # self.assertEqual(app.from_dir("SW"), (-1, 1))
+        # self.assertEqual(app.from_dir("S"), (0, 1))
+        # self.assertEqual(app.from_dir("E"), (1, 0))
+        pass
 
 
 if __name__ == '__main__':
--- woodapp.py
+++ woodapp.py
@@ -2,134 +2,219 @@
 import sys
 import math
 
+
+def get_raw():
+    "Обертка для системной функции, для отображения вводимых параметров"
+    raw = raw_input()
+    if False:
+        print >> sys.stderr, raw
+    return raw
+
+
+class GamePlayer(object):
+    "Описание возможных действий игрока"
+
+    def __init__(self, index, param):
+        "Инициируем информацию об игроке"
+        self.index = index
+        self.x, self.y = [int(j) for j in param.split()]
+        self.legals = []
+
+    def append_action(self, action):
+        "Добавляем в коллекцию список доступных действий"
+        self.legals.append(action)
+        return True
+
+    def position(self):
+        "Определяем текущую позицию игрока"
+        return self.x, self.y
+
+    def future_move(self, action):
+        "Вычисляем будующую позицию"
+        x, y = self.x, self.y
+        if action.atype == "MOVE&BUILD":
+            x, y  = x + action.dir_1[0], y + action.dir_1[1]
+
+        return x, y
+
+    def future_build(self, action):
+        "Вычисляем координату строительства"
+        x, y = self.x, self.y
+        if action.atype == "MOVE&BUILD":
+            x, y  = x + action.dir_1[0], y+action.dir_1[1]
+
+        x, y  = x + action.dir_2[0], y + action.dir_2[1]
+
+        return x, y
+
+    def __repr__(self):
+        return "%d: %d %d" % (self.index, self.x, self.y)
+
 class GameAction(object):
     "описание игровых действий"
     def __init__(self, param):
         "инициализация действия из командной строки"
-        pass
+        self.command = param
+        self.atype, index, dir_1, dir_2 = param.split()
+
+        self.index = int(index)
+
+        self.dir_1 = self.from_dir(dir_1)
+        self.dir_2 = self.from_dir(dir_2)
+
+    def from_dir(self, direction):
+        "Преобразуем координаты"
+        n, s, w, e = -1, 1, -1, 1
+        x, y = 0, 0
+        if "N" in direction:
+            y = n
+        if "S" in direction:
+            y = s
+        if "W" in direction:
+            x = w
+        if "E" in direction:
+            x = e
+
+        return x, y
+
+    def to_dir(self, coords):
+        "Преобразование обратно"
+        n, s, w, e = -1, 1, -1, 1
         
+        ret = ""
+        x, y = coords
+        if y == n:
+            ret = "N"
+        elif y == s:
+            ret = "S"
+        if x == w:
+            ret = ret + "W"
+        elif x == e:
+            ret = ret + "E"
+        return ret
+
+    def __repr__(self):
+        return "%s %d %s %s" % (self.atype, self.index, self.to_dir(self.dir_1), self.to_dir(self.dir_2))
+
 class World(object):
     "Описание игровой площадки"
     def __init__(self):
         "Начальные параметры игрового мира"
-        self.size = int(raw_input())
-        self.units_per_player = int(raw_input())
+
+        # управление параметрами вывода
+        self.show_input = True
+
+        self.size = int(get_raw())
+        self.units_per_player = int(get_raw())
 
         # описание игровой площадки
         self.grid = []
+        self.players = []
+        self.enemies = []
+        self.legals = {}
 
     def update(self):
         "Считавыем параметры из консоли"
 
         self.grid = []
+        self.legals = {}
+
 
         for _ in xrange(self.size):
-            row = raw_input()
-            self.grid.append(row)
+            self.grid.append(get_raw())
 
-        for _ in xrange(self.units_per_player):
-            players.append( [int(j) for j in raw_input().split()])
-            legals.append([])  
+        for i in xrange(self.units_per_player):
+            player = GamePlayer(i, get_raw())
+            self.players.append(player)
+            self.legals[i] = []
+
+        for i in xrange(self.units_per_player):
+            enemy = GamePlayer(i, get_raw())
+            self.enemies.append(enemy)
+
+        legal_actions = int(get_raw())
+
+        for i in xrange(legal_actions):
+            raw = get_raw()
+            atype, index, dir_1, dir_2 = raw.split()
+
+            index = int(index)
+            # распределяем список возможных действий
+            self.players[index].append_action(GameAction(raw))
 
     def maps(self):
         "отображаем карту в виде строки"
         ret = None
-        for row in grid:
-            if not (ret is None):
+        for row in self.grid:
+            if ret is not None:
                 ret = ret + "\n"
             else:
                 ret = ""
 
-            ret = ret + row
+            ret = ret + row.lstrip()
         return ret
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+    def available_move(self, next_pos):
+        "Проверяем, что клетка не занята противниками"
+        for player in self.players + self.enemies:
+            if player.position() == next_pos:
+                return False
+        return True
 
-def from_dir(direction):
-    n, s, w, e = -1, 1, -1, 1
-    x, y = 0, 0
-    if "N" in direction:
-        y = n
-    if "S" in direction:
-        y = s
-    if "W" in direction:
-        x = w
-    if "E" in direction:
-        x = e
-        
-    return x, y
-    
-def future_position(player, direction):
-    x, y = player
-    dx, dy = direction
-    return x + dx, y + dy
 
-def find_near_max(grid, player, legals):
-    
-    near = legals[0]
-    pos = future_position(player, from_dir(legals[0][2]))
-    h_near = height(grid, pos)
-    for atype, index, dir_1, dir_2 in legals:
-        pos = future_position(player, from_dir(dir_1))
-        h = height(grid, pos)
-        if h > h_near and h < 4:
-            h_near = h
-            near = (atype, index, dir_1, dir_2)
-    return near
-    
-    
-def height(grid, pos):
-    print >> sys.stderr, pos
-    x, y = pos
-    target = grid[y][x]
-    return target
-    
-def draw_map(grid):
-    ret = None
-    for row in grid:
-        if not (ret is None):
-            ret = ret + "\n"
-        else:
-            ret = ""
+    def height(self, forcell):
+        x, y = forcell
+        # переворачиваем координатную сетку
+        return self.grid[y][x]
+
         
-        ret = ret + row
-    return ret
+class StrategyWood(object):
+    """Стратегия для участия в деревянной лиге:
+    - ходит по доступным квадратам
+    """
+    def __init__(self, index, world):
+        "Инициализируем стратегию"
+        self.player = world.players[index]
+        self.world = world
+
+    def find_near_max(self):
+        "Ищем ближайшую точку"
+
+        near = self.player.legals[0]
+        pos = self.player.future_move(near)
+
+        h_near = self.world.height(pos)
+
+        for action in self.player.legals:
+            pos = self.player.future_move(action)
+
+            # определеяем доступность перемещения
+            if not self.world.available_move(pos):
+                continue
+
+            h = self.world.height(self.player.future_build(action))
+            if h > h_near and h < 4:
+                h_near = h
+                near = action
+        return near
+
+    def get_action(self):
+        return self.find_near_max()
+
 
 if __name__ == '__main__':
 
-    size = int(raw_input())
-    units_per_player = int(raw_input())
+    # size = int(raw_input())
+    # units_per_player = int(raw_input())
+    w = World()
 
     # game loop
     while True:
 
-        grid = []
-        legals = []
-        players = []
-        for i in xrange(size):
-            row = raw_input()
-            grid.append(row)
-        for i in xrange(units_per_player):
-            players.append( [int(j) for j in raw_input().split()])
-            legals.append([])
-        for i in xrange(units_per_player):
-            other_x, other_y = [int(j) for j in raw_input().split()]
-        legal_actions = int(raw_input())
-        for i in xrange(legal_actions):
-            raw = raw_input()
-            print >> sys.stderr, raw
+        w.update()
 
-            atype, index, dir_1, dir_2 = raw.split()
+        s1 = StrategyWood(0, w)
 
-            index = int(index)
-            legals[index].append((atype, index, dir_1, dir_2))
+        action = s1.get_action()
 
-        print >> sys.stderr, draw_map(grid)
-
-        for i in xrange(units_per_player):
-
-            player = players[i]
-
-            near = find_near_max(grid, player, legals[i])
-            print "%s %s %s %s" % near
+        print action
